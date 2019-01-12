619ff75fb8064f61227a7e5a6294e9f7b5f8d105
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-01-12 08:42:56 +0600

    JavaOutputFormatting

--- src/JavaOutputFormatting.java
+++ src/JavaOutputFormatting.java
@@ -4,54 +4,14 @@ public class JavaOutputFormatting {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        // taking input
-
-        String s1 = scanner.next();
-        int x1 = scanner.nextInt();
-
-        String s2 = scanner.next();
-        int x2 = scanner.nextInt();
-
-        String s3 = scanner.next();
-        int x3 = scanner.nextInt();
-
+        Scanner sc=new Scanner(System.in);
         System.out.println("================================");
-
-        System.out.print(s1 + "           ");
-
-        if(x1>99) {
-            System.out.println(x1);
-        } else if (x1 > 9 && x1 < 100) {
-            System.out.println("0" + x1);
-
-        } else if (x1 < 10) {
-            System.out.println("00" + x1);
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            System.out.printf("%-15s%03d%n", s1, x);
         }
-
-        System.out.print(s2 + "           ");
-
-        if(x2>99) {
-            System.out.println(x2);
-        } else if (x2 > 9 && x2 < 100) {
-            System.out.println("0" + x2);
-
-        } else if (x2 < 10) {
-            System.out.println("00" + x2);
-        }
-
-        System.out.print(s3 + "           ");
-
-        if(x3>99) {
-            System.out.println(x3);
-        } else if (x3 > 9 && x3 < 100) {
-            System.out.println("0" + x3);
-
-        } else if (x3 < 10) {
-            System.out.println("00" + x3);
-        }
-
         System.out.println("================================");
     }
 }
